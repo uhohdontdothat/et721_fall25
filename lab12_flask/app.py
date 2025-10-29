54c3dafba520b526ad080261754f8683fac82228
uhohdontdothat <henry256388792@gmail.com>
2025-10-29 14:49:32 +0000

    Progress on lab 12 made

--- lab12_flask/app.py
+++ lab12_flask/app.py
@@ -3,7 +3,7 @@ Henry Perez
 October 27th, 2025
 Lab 12, Introduction to Flask
 """
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 """
 create an object 'app' from the Flask module
@@ -15,7 +15,9 @@ app = Flask(__name__)
 # 'route' decorator is used to access the root URL
 @app.route('/')
 def index():
-    return render_template('index.html')
+    name = 'Henry Perez'
+    fruits = ['apple', 'orange', 'grapes']
+    return render_template('index.html',username = name, fruits_list = fruits)
 
 @app.route('/about')
 def about():
@@ -24,6 +26,10 @@ def about():
 @app.route('/quotes')
 def quotes():
     return '<h1>Quotes<h1>'
+
+@app.route('/login')
+def login():
+    return redirect(url_for('index'))
 
 # set the 'app' to run if you execute the file directly (not when it is imported)
 if __name__ == "__main__":
